2e241ac6146e72d38aad2fef6cb4c7fde04f38e3
cloud9291 <masudabdulyaqeen97@gmail.com>
2023-11-30 08:23:51 -0800

    cohort2

---
+++ helloworld.py
@@ -0,0 +1,34 @@
+#check the python version you are using
+import platform
+print("Python version:")
+print(platform.python_version())
+
+'''Open the python interactive shell and do the following operatons. 
+the operands are 3 and 4'''
+print(3 + 4)             # addition(+)
+print(4 - 3)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(4 / 3)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(4 % 3)             # modulus(%)
+print(4 // 3)            # Floor division operator(//)
+
+#Write strings on the python interactive shell. the strings are the following:
+name = "Masud"
+family_name = "Akogun"
+country = "Nigeria"
+
+print(name)
+print(family_name)
+print(country)
+print("I am enjoying 30 days of python")
+
+# Check the data types of the following data:
+print(type(10))          # Int
+print(type(9.8))         #double
+print(type(3.14))        # Float
+print(type(4 - 4j))      # Complex number
+print(type(['Asabeneh', 'Python', 'Finland']))  # list
+print(type('Masud')) # string
+print(type('Akagun')) # string
+print(type('Nigeria')) # string
